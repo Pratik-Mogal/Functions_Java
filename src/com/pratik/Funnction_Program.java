f153f5cf2c14dcc035de2ff72e8e043815ffb484
Pratik-Mogal <mogalpratik983@gmail.com>
2022-09-19 12:36:07 +0530

    Added the Comments

--- src/com/pratik/Funnction_Program.java
+++ src/com/pratik/Funnction_Program.java
@@ -1,2 +1,20 @@
-package com.pratik;public class Funnction_Program {
+package com.pratik;
+
+import java.util.Scanner;
+
+public class Funnction_Program {
+    public static void main(String[] args){
+        sum();
+    }
+    static void sum(){     // return_type name()
+        Scanner in = new Scanner(System.in); //body
+        System.out.println(" Enter Number 1: ");
+        int num1 = in.nextInt();
+        System.out.println(" Enter Number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The Sum = " + sum);
+
+    }
+  //return statement
 }
