c90c73fd3aa08f9a11604d6fa02ddc04825e3eb1
Pratik-Mogal <mogalpratik983@gmail.com>
2022-09-19 12:56:31 +0530

    Added the Comments : StringExample.java

--- src/com/pratik/Funnction_Program.java
+++ src/com/pratik/Funnction_Program.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class Funnction_Program {
     public static void main(String[] args){
-        sum();
+        int addition = sum();
+       System.out.println( "The sum of two Numbers is : " + addition);
     }
-    static void sum(){     // return_type name()
+    static int sum( /* We can add pramenters  here */){     // return_type name()
         Scanner in = new Scanner(System.in); //body
         System.out.println(" Enter Number 1: ");
         int num1 = in.nextInt();
         System.out.println(" Enter Number 2: ");
         int num2 = in.nextInt();
         int sum = num1 + num2;
-        System.out.println("The Sum = " + sum);
+        return sum;
 
     }
   //return statement
 }
+//What is return type : When we call the function what is the value of that function
---
+++ src/com/pratik/Greeting.java
@@ -0,0 +1,11 @@
+package com.pratik;
+
+public class Greeting {
+    public static void main(String[] args){
+        greeting();   //Function
+
+    }
+    static void greeting(){
+        System.out.println("Hello World ");
+    }
+}
