f34211da9c8a24d7ca144f7bf3206cd8e9cc228b
Pratik-Mogal <mogalpratik983@gmail.com>
2022-09-20 21:05:32 +0530

    Added the Comments : StringExample.java

---
+++ src/com/pratik/ChangeValue.java
@@ -0,0 +1,14 @@
+package com.pratik;
+import java.util.Arrays;
+public class ChangeValue {
+    public static void main(String[] args) {
+        // create an array
+        int[] arr ={1, 3, 2, 45, 6};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void change(int[] nums){
+        nums[0] = 99; //If we make a change to the object via this ref variable, same object will be change.
+    }
+}
